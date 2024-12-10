7d7f96ca73a80363ccdf7ca3d3640160cd3cfdec
Kevindecastro <9830@holbertonstudents.com>
2024-12-10 15:11:09 +0100

    add the prototype void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)) in the file 8-binary_tree_postorder.c it's a function that measures the depth of a node in a binary tree

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,1 +1,19 @@
-1
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Traverse un arbre binaire en post-ordre
+ * @tree: Pointeur vers la racine de l'arbre à traverser
+ * @func: Pointeur vers une fonction à appeler pour chaque nœud
+ *
+ * Description: La fonction applique `func` au nœud courant après avoir
+ * traversé le sous-arbre gauche et le sous-arbre droit.
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	return;
+
+	binary_tree_postorder(tree->left, func);/* Traverse le sous-arbre gauche */
+	binary_tree_postorder(tree->right, func);/* Traverse le sous-arbre droit */
+	func(tree->n);/* Applique la fonction au nœud courant */
+}
