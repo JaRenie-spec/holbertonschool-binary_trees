98936d1c20ead1b7380e66fa931275ba2cd6877a
Kevindecastro <9830@holbertonstudents.com>
2024-12-10 16:00:59 +0100

    add the prototype int binary_tree_balance(const binary_tree_t *tree) in the file 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,25 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Mesure la hauteur d'un arbre binaire
- * @tree: Pointeur vers le nœud racine de l’arbre
- *
- * Return: Hauteur de l’arbre, ou 0 si tree est NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-	return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
-}
-
-/**
  * binary_tree_balance - Calcule le facteur d’équilibre d’un arbre binaire
  * @tree: Pointeur vers le nœud racine de l’arbre
  *
