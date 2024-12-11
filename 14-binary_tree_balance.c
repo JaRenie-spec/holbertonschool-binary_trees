8327d67ccb2a7ce03049cefbd536ab541ecfdf6b
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-11 13:47:36 +0100

    implementing task 9 directly in it and change return value

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,28 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: 0 if NULL.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int leftheight, rightheight;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	leftheight = binary_tree_height(tree->left);
+	rightheight = binary_tree_height(tree->right);
+
+	if (leftheight >= rightheight)
+		return (leftheight + 1);
+	else
+		return (rightheight + 1);
+}
 
 /**
  * binary_tree_balance - function that measures the balance factor
@@ -10,6 +33,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+
 	if (tree == NULL)
 	return (0);
 
